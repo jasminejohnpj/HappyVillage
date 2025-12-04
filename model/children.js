38f6d549897a9a539146090a58ce4ee6caa5c31d
sebastian <sebastianseban0105@gmail.com>
2025-12-04 10:52:38 +0530

    droppedClass changed to string

--- model/children.js
+++ model/children.js
@@ -47,7 +47,7 @@ const ChildSchema = new mongoose.Schema(
     EducationalQualification: { type: String, trim: true, default: "" },
     otherEducationalQualification: { type: String, trim: true, default: "" },
     CurrentlyStudying: { type: String, trim: true, default: "" },
-    DroppedClass: { type: Boolean, default: false },
+    DroppedClass: { type: String, trim: true, default: "" },
     TerminateWithoutCompletion: { type: Boolean, default: false },
     CurrentlyWorking: { type: Boolean, default: false },
     Reason: { type: String, trim: true, default: "" },
