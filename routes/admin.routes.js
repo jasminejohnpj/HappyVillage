51f4b7d4b881cc8a22b94fb502f6e7001446dc48
sebastian <sebastianseban0105@gmail.com>
2025-12-05 14:50:53 +0530

    fixed admin login

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,13 +2,14 @@ import express from "express";
 import { registerSuperAdmin, loginSuperAdmin, allCounts, getWardNumbers, wardDetails , familyDetailsinWardwise,
     houseDetails ,personalDetails, searchHouse , filterValues, 
 } from "../controllers/admin.js";
-import { generateToken } from "../middleware/generatetoken.js";
+
 
 
 const adminRouter = express.Router();
 
+adminRouter.post("/superLogin", loginSuperAdmin);
 adminRouter.post("/superAdmin", registerSuperAdmin);
-adminRouter.post("/superLogin", loginSuperAdmin , generateToken);
+
 adminRouter.get("/allCounts", allCounts);
 adminRouter.get("/WardNo",getWardNumbers);
 adminRouter.get("/wardDetails", wardDetails);
